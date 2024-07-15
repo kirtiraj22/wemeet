ac65590e2e617bc678b7a31d3c6a4455a08ae294
kirtiraj22 <kirtirajthakor064@gmail.com>
2024-07-15 12:47:43 +0530

    frontend: added meeting cards

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,8 +27,19 @@ const config = {
           1: '#0E78F9',
         },
         sky: {
-          1: '#C9DDFF'
+          1: '#C9DDFF',
+          2: '#ECF0FF',
+          3: '#F5FCFF'
         },
+        orange: {
+          1: '#FF742E'
+        },
+        purple: {
+          1: '#830EF9'
+        },
+        yellow: {
+          1: '#F9A90E'
+        }
       },
       backgroundImage: {
         hero: "url('/images/hero-background.png')"
